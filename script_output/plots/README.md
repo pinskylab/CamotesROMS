3037b35d02addc78251a44e487f7818b1141394f
Malin Pinsky <malin.pinsky@gmail.com>
2024-03-07 17:05:13 -0800

    Plot LL differences by site (new Fig S5).

--- script_output/plots/README.md
+++ script_output/plots/README.md
@@ -1,6 +1,7 @@
 # script_output/plots
 
 - BootstrappedLikelihoodComparisonBothTimeScalesHistogramSubSampleSim.pdf: Figure 4 in the manuscript
-- Camotes_EKE_Avg.pdf: Figure S2 in the manuscript. Plot of eddy kinetic energy by site by years and monsoon. Produced by scripts/plotEKE_FigS2.R
+- Camotes_EKE_Avg.pdf: Figure S4 in the manuscript. Plot of eddy kinetic energy by site by years and monsoon. Produced by scripts/plotEKE_FigS2.R
 - GenPlusSimKernels.pdf: Figure 5 in the manuscript
-- GenPlusSimMDD.pdf: Figure S3 in the manuscript
+- GenPlusSimMDD.pdf: Figure 64 in the manuscript. Mean dispersal distances from observed and simulated dispersal kernels.
+- loglikelihood_differences_bysite.png: Figure S5 in the manuscript
--- scripts/examine_LL_by_site.R
+++ scripts/examine_LL_by_site.R
@@ -38,55 +38,48 @@ genetic_parentage_matrices <- suppressMessages(format_genetic_parentage_matrix(A
 Data_par2012_bio2012 <- list(BioPhysMat=as.matrix(biophys_norm_matrices$AnnualBiophysMatNorm2012[1:nrow(sampled_reefs_vec),]), 
                              Assignments=genetic_parentage_matrices$GenMat2012[1:nrow(genetic_parentage_matrices$GenMat2012)-1,], 
                              pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                             sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), 
+                             sampled_reefs_vec=sampled_reefs_vec, 
                              prop_samp_vec=as.matrix(SurveyData[year == 2012, .(prop_anem_samp)]), 
                              unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2012[nrow(genetic_parentage_matrices$GenMat2012),]))
 Data_par2013_bio2013 <- list(BioPhysMat=as.matrix(biophys_norm_matrices$AnnualBiophysMatNorm2013[1:nrow(sampled_reefs_vec),]), 
                              Assignments=genetic_parentage_matrices$GenMat2013[1:nrow(genetic_parentage_matrices$GenMat2013)-1,], 
                              pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                             sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), 
+                             sampled_reefs_vec=sampled_reefs_vec, 
                              prop_samp_vec=as.matrix(SurveyData[year == 2013,  .(prop_anem_samp)]), 
                              unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2013[nrow(genetic_parentage_matrices$GenMat2013),]))
 Data_par2014_bio2014 <- list(BioPhysMat=as.matrix(biophys_norm_matrices$AnnualBiophysMatNorm2014[1:nrow(sampled_reefs_vec),]), 
                              Assignments=genetic_parentage_matrices$GenMat2014[1:nrow(genetic_parentage_matrices$GenMat2014)-1,], 
                              pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                             sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
+                             sampled_reefs_vec=sampled_reefs_vec, 
+                             prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
                              unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2014[nrow(genetic_parentage_matrices$GenMat2014),]))
 Data_par2012_4_bio2012_4 <- list(BioPhysMat=as.matrix(biophys_norm_matrices$FullBiophysMatNorm[1:nrow(sampled_reefs_vec),]), 
                                  Assignments=genetic_parentage_matrices$GenMat2012_4[1:nrow(genetic_parentage_matrices$GenMat2012_4)-1,], 
                                  pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                                 sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
+                                 sampled_reefs_vec=sampled_reefs_vec, 
+                                 prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
                                  unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2012_4[nrow(genetic_parentage_matrices$GenMat2012_4),]))
 Data_parNEM_bioNEM <- list(BioPhysMat=as.matrix(biophys_norm_matrices$MonsoonBiophysMatNormNEM[1:nrow(sampled_reefs_vec),]), 
                            Assignments=as.matrix(genetic_parentage_matrices$GenMatNEM[1:nrow(genetic_parentage_matrices$GenMatNEM)-1,]), 
                            pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                           sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
+                           sampled_reefs_vec=sampled_reefs_vec, 
+                           prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
                            unassigned_vec=as.matrix(genetic_parentage_matrices$GenMatNEM[nrow(genetic_parentage_matrices$GenMatNEM),]))
 Data_parSWM_bioSWM <- list(BioPhysMat=as.matrix(biophys_norm_matrices$MonsoonBiophysMatNormSWM[1:nrow(sampled_reefs_vec),]), 
                            Assignments=as.matrix(genetic_parentage_matrices$GenMatSWM[1:nrow(genetic_parentage_matrices$GenMatSWM)-1,]), 
                            pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                           sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
+                           sampled_reefs_vec=sampled_reefs_vec, 
+                           prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
                            unassigned_vec=as.matrix(genetic_parentage_matrices$GenMatSWM[nrow(genetic_parentage_matrices$GenMatSWM),]))
 Data_parNEMSWM_bio2012_4 <- list(BioPhysMat=as.matrix(biophys_norm_matrices$FullBiophysMatNorm[1:nrow(sampled_reefs_vec),]), 
                                  Assignments=(as.matrix(genetic_parentage_matrices$GenMatNEM[1:nrow(genetic_parentage_matrices$GenMatNEM)-1,])+ as.matrix(genetic_parentage_matrices$GenMatSWM[1:nrow(genetic_parentage_matrices$GenMatSWM)-1,])), 
                                  pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), 
-                                 sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
+                                 sampled_reefs_vec=sampled_reefs_vec, 
+                                 prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), 
                                  unassigned_vec=as.matrix(genetic_parentage_matrices$GenMatNEM[nrow(genetic_parentage_matrices$GenMatNEM),])+as.matrix(genetic_parentage_matrices$GenMatSWM[nrow(genetic_parentage_matrices$GenMatSWM),]))
 
-## Set up data vs. model comparisons for each year, monsoon, and multi-period averages (subsampled data)
-SimConnInt2SubSample <- SimConn[!is.na(dest_num_rec_sampled_annual), .SD[sample(.N, unique(dest_num_rec_sampled_annual), replace=TRUE)], by=c("year", "dest")]
-SimConnInt2SubSamplePar <- SimConnInt2SubSample[!is.na(dest_num_rec_sampled_annual)& source !="unknown", .SD[sample(.N, unique(num_annual_parentage_matches), replace=TRUE)], by="year"]
-SimConnInt2SubSample[particle_id %in% SimConnInt2SubSamplePar[, particle_id], source := "unknown"]
-biophys_norm_matrices_subsample <- format_biophys_normalized_matrix(SimConn=SimConnInt2SubSample, total_release_days=687, AddDestSim = AddDestSim)
-Data_par2012_bio2012_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$AnnualBiophysMatNorm2012[1:nrow(sampled_reefs_vec),]), Assignments=genetic_parentage_matrices$GenMat2012[1:nrow(genetic_parentage_matrices$GenMat2012)-1,], pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2012,  .(prop_anem_samp)]), unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2012[nrow(genetic_parentage_matrices$GenMat2012),]))
-Data_par2013_bio2013_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$AnnualBiophysMatNorm2013[1:nrow(sampled_reefs_vec),]), Assignments=genetic_parentage_matrices$GenMat2013[1:nrow(genetic_parentage_matrices$GenMat2013)-1,], pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2013,  .(prop_anem_samp)]), unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2013[nrow(genetic_parentage_matrices$GenMat2013),]))
-Data_par2014_bio2014_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$AnnualBiophysMatNorm2014[1:nrow(sampled_reefs_vec),]), Assignments=genetic_parentage_matrices$GenMat2014[1:nrow(genetic_parentage_matrices$GenMat2014)-1,], pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2014[nrow(genetic_parentage_matrices$GenMat2014),]))
-Data_par2012_4_bio2012_4_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$FullBiophysMatNorm[1:nrow(sampled_reefs_vec),]), Assignments=genetic_parentage_matrices$GenMat2012_4[1:nrow(genetic_parentage_matrices$GenMat2012_4)-1,], pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), unassigned_vec=as.matrix(genetic_parentage_matrices$GenMat2012_4[nrow(genetic_parentage_matrices$GenMat2012_4),]))
-Data_parNEM_bioNEM_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$MonsoonBiophysMatNormNEM[1:nrow(sampled_reefs_vec),]), Assignments=as.matrix(genetic_parentage_matrices$GenMatNEM[1:nrow(genetic_parentage_matrices$GenMatNEM)-1,]), pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]),  unassigned_vec=as.matrix(genetic_parentage_matrices$GenMatNEM[nrow(genetic_parentage_matrices$GenMatNEM),]))
-Data_parSWM_bioSWM_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$MonsoonBiophysMatNormSWM[1:nrow(sampled_reefs_vec),]), Assignments=as.matrix(genetic_parentage_matrices$GenMatSWM[1:nrow(genetic_parentage_matrices$GenMatSWM)-1,]), pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]),  unassigned_vec=as.matrix(genetic_parentage_matrices$GenMatSWM[nrow(genetic_parentage_matrices$GenMatSWM),]))
-Data_parNEMSWM_bio2012_4_subsample <- list(BioPhysMat=as.matrix(biophys_norm_matrices_subsample$FullBiophysMatNorm[1:nrow(sampled_reefs_vec),]), Assignments=(as.matrix(genetic_parentage_matrices$GenMatNEM[1:nrow(genetic_parentage_matrices$GenMatNEM)-1,])+as.matrix(genetic_parentage_matrices$GenMatSWM[1:nrow(genetic_parentage_matrices$GenMatSWM)-1,])), pop_size_vec=as.matrix(SurveyData[,.(avg_num_females=mean(num_females, na.rm = TRUE)), by=site][order(site)][, .(avg_num_females)]), sampled_reefs_vec=as.matrix(SiteIndexBioPhys[site %in% SurveyData[, site], .(index)]), prop_samp_vec=as.matrix(SurveyData[year == 2014,  .(prop_anem_samp)]), unassigned_vec=as.matrix(genetic_parentage_matrices$GenMatNEM[nrow(genetic_parentage_matrices$GenMatNEM),])+as.matrix(genetic_parentage_matrices$GenMatSWM[nrow(genetic_parentage_matrices$GenMatSWM),]))
 
-## Log likelilhoods for the full data, separately by site
+## Log likelihoods for the full data, separately by site
 LL_annual_annual_2012 <- LL_biophys_bysite(Data_par2012_bio2012) # year-specific
 LL_annual_annual_2013 <- LL_biophys_bysite(Data_par2013_bio2013) # year-specific
 LL_annual_annual_2014 <- LL_biophys_bysite(Data_par2014_bio2014) # year-specific
@@ -95,18 +88,20 @@ LL_monsoonal_monsoonal_NEM <- LL_biophys_bysite(Data_parNEM_bioNEM)
 LL_monsoonal_monsoonal_SWM <- LL_biophys_bysite(Data_parSWM_bioSWM) # monsoon-specific
 LL_monsoonal_total <- LL_biophys_bysite(Data_parNEMSWM_bio2012_4) # cross-monsoon average
 
+## LL differences by site
+LL_byyear = LL_annual_annual_2012 + LL_annual_annual_2013 + LL_annual_annual_2014
+LLdiff_ave_vs_annual = LL_byyear - LL_total_total # positive values indidate sites where the year-specific sims are better than the average
+LLdiff_ave_vs_monsoon = LL_monsoonal_monsoonal_NEM + LL_monsoonal_monsoonal_SWM- LL_monsoonal_total # positive values indidate sites where the monsoon-specific sims are better than the average
+
+
 ## barplot of negative log-likelihoods. lower is better fit. normalized to min 0
-toplot <- matrix(data = c(-LL_annual_annual_2012 + max(LL_annual_annual_2012),
-                          -LL_annual_annual_2013 + max(LL_annual_annual_2013),
-                          -LL_annual_annual_2014 + max(LL_annual_annual_2014),
-                          -LL_total_total + max(LL_total_total),
-                          -LL_monsoonal_monsoonal_NEM + max(LL_monsoonal_monsoonal_NEM),
-                          -LL_monsoonal_monsoonal_SWM + max(LL_monsoonal_monsoonal_SWM),
-                          -LL_monsoonal_total + max(LL_monsoonal_total)), 
-                 ncol=18, byrow = TRUE, dimnames = list(c('2012', '2013', '2014', 'Annual average', 'NEM', 'SWM', 'Monsoonal average'), SiteIndexBioPhys$site[sampled_reefs_vec]))
+toplot <- matrix(data = c(LLdiff_ave_vs_annual,
+                          LLdiff_ave_vs_monsoon), 
+                 ncol=18, byrow = TRUE, dimnames = list(c('Annual', 'Monsoonal'), SiteIndexBioPhys$site[sampled_reefs_vec]))
+toplot = toplot[,c('Palanas', 'Wangag', 'Magbangon', 'Cabatoan', 'Caridad Cemetery', 'Caridad Proper', 'Hicgop South', 'Sitio Tugas', 'Elementary School', 'Sitio Lonas', 'San Agustin', 'Poroc San Flower', 'Poroc Rose', 'Visca', 'Gabas', 'Tamakin Dacot', 'Haina', 'Sitio Baybayon')]
 
-# plot. next: order these N to S
-barplot(toplot, beside = TRUE, las = 2)
-par(mar=c(15,4,2,2)); barplot(toplot, beside = FALSE, las = 2)
-
-    
+# plot 
+png(filename = 'script_output/plots/loglikelihood_differences_bysite.png', width=6, height = 6, units = 'in', res = 300)
+par(mar=c(10,4,2,2)); barplot(toplot, beside = TRUE, las = 2, ylab = 'Difference in log likelihood', col = c('gray90', 'gray30'))
+legend(1,1200, legend = c('Annual', 'Monsoonal'), fill = c('gray90', 'gray30'))
+dev.off()
